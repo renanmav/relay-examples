a741c536f8c3157857cffea149891d8c2cd21fa8
Renan Machado <renan.mav@hotmail.com>
2019-11-02 21:41:20 -0300

    add fb header

--- redo-todo/js/ErrorBoundaryWithRetry.js
+++ redo-todo/js/ErrorBoundaryWithRetry.js
@@ -1,10 +1,32 @@
-import React from 'react';
+// @flow
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-class ErrorBoundaryWithRetry extends React.Component {
+import React, {type Node} from 'react';
+
+type State = {|
+  error: ?Error,
+|};
+
+type Props = {|
+  fallback: Function,
+  children: Node,
+|};
+
+class ErrorBoundaryWithRetry extends React.Component<Props, State> {
   state = {error: null};
 
-  static getDerivedStateFromError(error) {
-    return {error: error};
+  static getDerivedStateFromError(error: Error) {
+    return {error};
   }
 
   _retry = () => {
--- redo-todo/js/components/TodoRoot.js
+++ redo-todo/js/components/TodoRoot.js
@@ -1,4 +1,16 @@
 // @flow
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import React from 'react';
 
 import {useLazyLoadQuery, graphql} from 'react-relay/hooks';
@@ -6,6 +18,7 @@ import {useLazyLoadQuery, graphql} from 'react-relay/hooks';
 import type {TodoRootQueryResponse} from 'relay/TodoRootQuery.graphql';
 
 import TodoApp from './TodoApp';
+import ErrorBoundaryWithRetry from '../ErrorBoundaryWithRetry';
 
 const TodoRoot = () => {
   const {user}: TodoRootQueryResponse = useLazyLoadQuery(
@@ -25,9 +38,11 @@ const TodoRoot = () => {
 const Loading = () => <div>Loading</div>;
 
 const TodoRootWrapper = () => (
-  <React.Suspense fallback={<Loading />}>
-    <TodoRoot />
-  </React.Suspense>
+  <ErrorBoundaryWithRetry fallback={error => <div>{error.message}</div>}>
+    <React.Suspense fallback={<Loading />}>
+      <TodoRoot />
+    </React.Suspense>
+  </ErrorBoundaryWithRetry>
 );
 
 export default TodoRootWrapper;
